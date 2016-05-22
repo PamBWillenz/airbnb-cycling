72e0c705552ca721ff416d2888ba8079f0fc0e47
Pam Willenz <pam@positek.net>
2016-05-22 14:54:06 -0400

    modified rails_helper.rb for capybara-webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'simple_bdd'
 require 'shoulda/matchers'
+Capybara.javascript_driver = :webkit
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -35,31 +36,28 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
-  config.include SimpleBdd, type: :feature
   # config.include Devise::TestHelpers, :type => :controller
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean_with(:truncation)
-  end
+config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
+    end 
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
+    config.before(:each) do
+      DatabaseCleaner.start
     end
-  end
 
-  Capybara.javascript_driver = :webkit
-  
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
+config.include SimpleBdd, type: :feature
+    
+    Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+      end
+    end  
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
