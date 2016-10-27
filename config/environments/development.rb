e5d37788edbf09ed79f9862b9106acd2f80c8739
Pam Willenz <pam@positek.net>
2016-10-27 11:48:11 -0400

    Installed imagemagick, added to profile_spec.rb, installed paperclip gem 5.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # ImageMagick config for Paperclip
+  Paperclip.options[:command_path] = "/usr/local/Cellar/"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -13,7 +13,11 @@ feature "profiles" do
     visit new_member_profile_path(member)
     expect(page).to have_content "Profiles"
     fill_in "profile[bio]", with: FFaker::Lorem.paragraph(2)
+    profile_pic_path = 'spec/fixtures/files/profile_pic.jpg'
+    attach_file "profile[profile_pic]", profile_pic_path
     click_button "Create Profile"
+    expect(profile).to have_attributes(profile_pic_file_name: a_value)
+    profile = Profile.last
     expect(page).to have_content("You successfully created your profile.")
   end
 end
