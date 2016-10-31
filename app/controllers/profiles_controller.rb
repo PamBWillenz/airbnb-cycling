9793be92d76d41e1ccb29416222ed2bc0a1b58ba
Pam Willenz <pam@positek.net>
2016-10-31 11:01:19 -0400

    Trying to fix error and get profile link/form to show up

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,7 @@ class ProfilesController < ApplicationController
   def create
     @profile = @member.create_profile(profile_params)
     if @profile.save
-      redirect_to member_profile_path(current_member, @profile), notice: "You successfully created your profile."
+      redirect_to member_profile_path(@member, @profile), notice: "You successfully created your profile."
     else
       flash[:alert] = "Could not create your profile."
       render :new
@@ -26,7 +26,7 @@ class ProfilesController < ApplicationController
   def update
     @profile = Profile.find(params[:id])
     if @profile.update(profile_params)
-      redirect_to member_profile_path(current_member, @profile), notice: "You successfully updated your profile."
+      redirect_to member_profile_path(@member, @profile), notice: "You successfully updated your profile."
     else
       flash[:alert] = "Could not update profile."
       render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :members
-  root 'home#index'
 
   resources :members do
     resources :profiles
   end
+  
+  root 'home#index'
+  
 end
