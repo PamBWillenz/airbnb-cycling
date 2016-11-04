2dc9185ba26d043c3ca3d0dc8041ba0972f9a2b3
Pam Willenz <pam@positek.net>
2016-11-04 15:44:52 -0400

    Still fixing spec/features/member_views_profile_spec.rb failing test

--- spec/features/member_views_profile_spec.rb
+++ spec/features/member_views_profile_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 feature "member views profile" do 
-  let(:member) {FactoryGirl.create(:member)}
+  let(:profile) {FactoryGirl.create(:profile)}
 
   before do
-    login_as(member, :scope => :member)
+    login_as(profile.member, :scope => :member)
   end
 
   scenario "visiting index page of profiles" do 
@@ -13,17 +13,18 @@ feature "member views profile" do
     expect(page).to have_content "Traveling Cyclist Profiles"
     expect(page).to have_content(:profile)
 
-    expect(page).to have_link("View Profile", href: member_profile_path)
+    expect(page).to have_link("View Profile", href: member_profile_path(profile.member, profile))
   end
 
   scenario "visiting profile show page" do 
     visit profiles_path
 
-    find("a[href='#{member_profile_path}']").click
+    find("a[href='#{member_profile_path(profile.member, profile)}']", :match => :first).click 
 
-    expect(page).to have_content "#{member.name}'s Profile"
+    expect(page).to have_content "#{profile.member.name}'s Profile"
     expect(page).to have_content profile.bio
-    expect(page).to have_content "profile[profile_pic]", profile_pic_path
+    expect(page).to have_content "profile[profile_pic]", @profile.profile_pic # matches the path on the profile#show
+
   end
 
 end
