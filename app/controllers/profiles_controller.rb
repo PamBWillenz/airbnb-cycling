97fe7ce53b532b35123674acd173387ac578b43e
Pam Willenz <pam@positek.net>
2016-11-08 09:38:21 -0500

    Hid pundit authorize for profiles#controller#index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   before_action :authenticate_member!
 
   def index
-    authorize @profile
+    # authorize @profile
     @profiles = Profile.all 
   end
 
