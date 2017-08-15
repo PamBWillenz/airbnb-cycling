11561bfe0be7a97c2b2093e31ed7f595e5df36aa
Pam Willenz <pam@positek.net>
2017-08-15 18:45:54 -0400

    change to test codeship deployment to heroku

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -3,8 +3,6 @@ $(function() {
     $('#datepicker-end').pickadate()
 });
 
-// REGULAR VERSION
-
 // $(document).ready('turbolinks:load', function() { 
 //   jQuery(function($) {
 //     $("#datepicker-start").pickadate({
