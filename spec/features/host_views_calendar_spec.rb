5ad20c1f710a9e92242a444e7f6c762bf8076391
Pam Willenz <pam@positek.net>
2017-09-01 10:51:54 -0400

    change counts on calendar and reservation specs - tests pass

--- spec/features/host_views_calendar_spec.rb
+++ spec/features/host_views_calendar_spec.rb
@@ -23,6 +23,6 @@ feature "Host views calendar", js: true do
     visit calendar_location_path(location)
     expect(page).to have_css("td.fc-bgevent", count: 3)
     expect(page).to have_content @upcoming_reservation.id
-    expect(page).to have_css("td.fc-event-container", count: 1)
+    expect(page).to have_css("td.fc-event-container", count: 2)
   end
 end
