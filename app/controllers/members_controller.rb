8646f1876b234c2c4846527df16b68f68950dd9d
Pam Willenz <pam@positek.net>
2017-09-12 21:00:48 -0400

    fix payout_account to redirect if already a host

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,5 +2,9 @@ class MembersController < ApplicationController
   before_action :authenticate_member!
 
   def payout_account
+    if current_member.stripe_user_id.present?
+      redirect_to host_locations_path, alert:
+        "You have already connected your Stripe Account! Be sure to create a new location."
+    end
   end
 end
