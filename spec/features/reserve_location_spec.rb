9223f91e1a1727483441d13c0032cc21de2fd693
Pam Willenz <pam@positek.net>
2017-10-02 13:46:06 -0400

    add stripe error tests to reserve#location#spec, tests failing--skip-ci

--- spec/features/reserve_location_spec.rb
+++ spec/features/reserve_location_spec.rb
@@ -10,46 +10,174 @@ feature "Member reserves a location" do
     FactoryGirl.create(:profile_with_pic, member: host)
   end
 
-  scenario "by visiting location show page and selecting dates", js: true do
-    visit location_path(location)
-    expect(page).to have_content location.description
-    expect(page).to have_content location.member.profile.bio
-  
-    execute_script("
-      $('#datepicker-start').pickadate('picker').set(
-        'select', new Date((new Date()).valueOf() + 1000*3600*24),
-        { format: 'yyyy-mm-dd'});"
-      )
-    execute_script("
-      $('#datepicker-end').pickadate('picker').set(
-        'select', new Date((new Date()).valueOf() + 1000*3600*48),
-        { format: 'yyyy-mm-dd'});"
-      )
+  context "with valid data" do
+    scenario "by visiting location show page and selecting dates", js: true do
+      visit location_path(location)
+      expect(page).to have_content location.description
+      expect(page).to have_content location.member.profile.bio
+    
+      execute_script("
+        $('#datepicker-start').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*24),
+          { format: 'yyyy-mm-dd'});"
+        )
+      execute_script("
+        $('#datepicker-end').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*48),
+          { format: 'yyyy-mm-dd'});"
+        )
 
-    click_button "Make a Reservation"
-    expect(page).to have_content "Reservation Summary:"
+      click_button "Make a Reservation"
+      expect(page).to have_content "Reservation Summary:"
 
-    fill_in "card_number", with: "4242 4242 4242 4242"
-    select "January"
-    select "2020"
-    fill_in "card_verification", with: "123"
-    fill_in "address_zip", with: "10001"
+      fill_in "card_number", with: "4242 4242 4242 4242"
+      select "January"
+      select "2020"
+      fill_in "card_verification", with: "123"
+      fill_in "address_zip", with: "10001"
 
-    click_button "Confirm Reservation"
+      click_button "Confirm Reservation"
 
-    expect(page).to have_content "Reservation successfully created."
-    expect(page).to have_content "THANKS FOR BOOKING WITH US!"
+      expect(page).to have_content "Reservation successfully created."
+      expect(page).to have_content "THANKS FOR BOOKING WITH US!"
 
-    expect(Reservation.count).to eq 1
-    reservation = Reservation.last
+      expect(Reservation.count).to eq 1
+      reservation = Reservation.last
 
-    expect(reservation).to have_attributes(id_for_credit_card_charge:
-      a_string_starting_with("ch"), location_id: location.id, member_id: member.id, 
-      start_date: Date.current + 1.day, end_date: Date.today + 2.days)
+      expect(reservation).to have_attributes(id_for_credit_card_charge:
+        a_string_starting_with("ch"), location_id: location.id, member_id: member.id, 
+        start_date: Date.current + 1.day, end_date: Date.today + 2.days)
 
-      first_available_date = AvailableDate.first 
-      second_available_date = AvailableDate.second 
-      expect(first_available_date.booked).to eq true
-      #expect(second_available_date.booked).to eq false
+        first_available_date = AvailableDate.first 
+        second_available_date = AvailableDate.second 
+        expect(first_available_date.booked).to eq true
+        #expect(second_available_date.booked).to eq false
+    end
+  end
+
+  context "with invalid data" do 
+    scenario "charge is declined", js: true do 
+      visit location_path(location)
+      expect(page).to have_content location.description
+      expect(page).to have_content location.member.profile.bio
+    
+      execute_script("
+        $('#datepicker-start').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*24),
+          { format: 'yyyy-mm-dd'});"
+        )
+      execute_script("
+        $('#datepicker-end').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*48),
+          { format: 'yyyy-mm-dd'});"
+        )
+
+      click_button "Make a Reservation"
+      expect(page).to have_content "Reservation Summary:"
+
+      fill_in "card_number", with: "4000 4000 4000 4002"
+      select "January"
+      select "2020"
+      fill_in "card_verification", with: "123"
+      fill_in "address_zip", with: "10001"
+
+      click_button "Confirm Reservation"
+      sleep 5
+      expect(page).to have_content "Your card was declined."
+    end
+
+    scenario "stripe returns error for invalid cvc-code", js: true do 
+      visit location_path(location)
+      expect(page).to have_content location.description
+      expect(page).to have_content location.member.profile.bio
+    
+      execute_script("
+        $('#datepicker-start').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*24),
+          { format: 'yyyy-mm-dd'});"
+        )
+      execute_script("
+        $('#datepicker-end').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*48),
+          { format: 'yyyy-mm-dd'});"
+        )
+
+      click_button "Make a Reservation"
+      expect(page).to have_content "Reservation Summary:"
+
+      fill_in "card_number", with: "4242 4242 4242 4242"
+      select "January"
+      select "2020"
+      fill_in "card_verification", with: "88"
+      fill_in "address_zip", with: "10001"
+
+      click_button "Confirm Reservation"
+      sleep 5
+
+      expect(page).to have_content "Your card's security code is invalid."
+    end
+
+    scenario "card has expired", js: true do 
+      visit location_path(location)
+      expect(page).to have_content location.description
+      expect(page).to have_content location.member.profile.bio
+    
+      execute_script("
+        $('#datepicker-start').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*24),
+          { format: 'yyyy-mm-dd'});"
+        )
+      execute_script("
+        $('#datepicker-end').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*48),
+          { format: 'yyyy-mm-dd'});"
+        )
+
+      click_button "Make a Reservation"
+      expect(page).to have_content "Reservation Summary:"
+
+      fill_in "card_number", with: "4000 4000 4000 0069"
+      select "January"
+      select "2020"
+      fill_in "card_verification", with: "123"
+      fill_in "address_zip", with: "10001"
+
+      click_button "Confirm Reservation"
+      sleep 5
+
+      expect(page).to have_content "Your card has expired."
+    end
+      
+
+    scenario "processing error", js: true do 
+      visit location_path(location)
+      expect(page).to have_content location.description
+      expect(page).to have_content location.member.profile.bio
+    
+      execute_script("
+        $('#datepicker-start').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*24),
+          { format: 'yyyy-mm-dd'});"
+        )
+      execute_script("
+        $('#datepicker-end').pickadate('picker').set(
+          'select', new Date((new Date()).valueOf() + 1000*3600*48),
+          { format: 'yyyy-mm-dd'});"
+        )
+
+      click_button "Make a Reservation"
+      expect(page).to have_content "Reservation Summary:"
+
+      fill_in "card_number", with: "4000 0000 0000 0119"
+      select "January"
+      select "2020"
+      fill_in "card_verification", with: "123"
+      fill_in "address_zip", with: "10001"
+
+      click_button "Confirm Reservation"
+
+      expect(page).to have_content "An error occurred while processing your card."
+    end
   end
 end
+      
