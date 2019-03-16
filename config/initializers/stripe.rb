4e3f3733ab8a914c425f3f82309c562b861af082
Pam Willenz <pam@positek.net>
2019-03-16 10:15:50 -0400

    put signing_secret_key in ENV and created another dispute - webhook work, test still fails

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Stripe.api_key = ENV["stripe_secret_key"]
-StripeEvent.signing_secret = "whsec_GZE4akyF9TH9g02wppEJtYpHK7PewWBC"
+StripeEvent.signing_secret = ENV["stripe_signing_key"]
 
 # StripeEvent.configure do |events| 
 #   events.subscribe 'charge.dispute.created' do |event| 
@@ -8,6 +8,6 @@ StripeEvent.subscribe 'charge.dispute.created' do |event|
   event.class       #=> Stripe::Event
   event.type        #=> "charge.dispute.created"
   event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
-  # end
+  #end
 CustomerDisputeMailer.send_admin_customer_dispute(event).deliver_now
 end
--- spec/mailers/previews/customer_dispute_preview.rb
+++ spec/mailers/previews/customer_dispute_preview.rb
@@ -3,7 +3,8 @@ class CustomerDisputePreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/customer_dispute/send_admin_customer_dispute
   def send_admin_customer_dispute
-    event = Stripe::Event.retrieve("evt_1ECpmMKGa7MvTac7UrgNRX6G")
+    event = Stripe::Event.retrieve("evt_1EEd1tKGa7MvTac7cvoKcpPk")
+    
     CustomerDisputeMailer.send_admin_customer_dispute(event)
   end
 end
